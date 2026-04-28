7f8063bb139e3a3bf24522f7f6bdbcc9d2b97a08
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-04-28 14:58:30 -0400

    Add: max-area-of-island - submission-2

---
+++ Data Structures & Algorithms/max-area-of-island/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    maxAreaOfIsland(grid) {
+        let max = 0;
+
+        for (let r = 0; r < grid.length; r++) {
+            for (let c = 0; c < grid[r].length; c++) {
+                if (grid[r][c] === 1) {
+                    const size = this.dfs(grid, r, c);
+                    max = Math.max(size, max);
+                }
+            }
+        }
+
+        return max;
+
+    }
+
+    dfs(grid, r, c) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+
+        if (r === rows || c === cols || c < 0 || r < 0 || grid[r][c] !== 1) {
+            return 0;
+        }
+
+        grid[r][c] = 0;
+
+        return (1 + this.dfs(grid, r + 1, c) + this.dfs(grid, r - 1, c) + this.dfs(grid, r, c + 1) + this.dfs(grid, r, c - 1));
+    }
+}
