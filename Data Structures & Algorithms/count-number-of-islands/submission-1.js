93c4a7987848217c0c252c43894f0731e7558252
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-04-28 14:00:17 -0400

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {character[][]} grid
+     * @return {number}
+     */
+    numIslands(grid) {
+        let res = 0;
+
+        for (let r = 0; r < grid.length; r++) {
+            for (let c = 0; c < grid[r].length; c++) {
+               if (grid[r][c] === "1") {
+                    this.dfs(grid, r, c);
+                    res++;
+               } 
+            }
+        }
+
+        return res;
+    }
+
+    
+    dfs(grid, r, c) {
+        const rows = grid.length;
+        const cols = grid[0].length; 
+        if (r < 0 || c < 0 || r === rows || c === cols || grid[r][c] === '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        this.dfs(grid, r + 1, c);
+        this.dfs(grid, r - 1, c);
+        this.dfs(grid, r, c + 1);
+        this.dfs(grid, r, c - 1);
+    }
+}
