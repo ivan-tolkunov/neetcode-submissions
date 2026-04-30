ae3fd016cdc61fbeebcb9359ab248a5551571bb4
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-04-30 12:24:51 -0400

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} text1
+     * @param {string} text2
+     * @return {number}
+     */
+    longestCommonSubsequence(text1, text2) {
+        const cache = new Map();
+        function dfs(i, j) {
+            if (i === text1.length || j === text2.length) {
+                return 0;
+            }
+
+            const key = `${i},${j}`;
+
+            if (cache.has(key)) {
+                return cache.get(key);
+            }
+
+            let memo;
+
+            if (text1[i] === text2[j]) {
+                memo = 1 + dfs(i+ 1, j + 1);
+            } else {
+                memo = Math.max(dfs(i + 1, j), dfs(i, j + 1));
+            }
+
+            cache.set(key, memo);
+
+            return memo;
+        }
+
+        return dfs(0, 0);
+    }
+}
