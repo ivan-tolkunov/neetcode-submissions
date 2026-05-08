6610cd4be7a18c9f81beabbe653ad0ef9bc1af6d
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-05-08 14:46:56 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    // 1 - 26
+    numDecodings(s) {
+        const cache = new Map();
+
+        function dfs (i) {
+            if (i === s.length) {
+                return 1;
+            }
+
+            if (s[i] === '0') {
+                return 0;
+            }
+
+            if (cache.has(i)) {
+                return cache.get(i);
+            }
+
+
+            let res = dfs(i + 1);
+
+            if (i + 1 < s.length && Number(s.slice(i, i + 2) <= 26)) {
+                res += dfs(i + 2);
+            }
+
+            cache.set(i, res);
+
+            return res;
+        }
+
+        return dfs(0);
+    }
+
+}
