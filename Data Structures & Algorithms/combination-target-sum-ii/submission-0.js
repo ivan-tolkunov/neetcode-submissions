eadae46e87af5b6cf2473c6adb29a6a1b9818045
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-05-19 15:02:01 -0400

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} candidates
+     * @param {number} target
+     * @return {number[][]}
+     */
+    combinationSum2(candidates, target) {
+        candidates.sort((a, b) => a - b);
+
+        const res = [];
+
+        function dfs(start, sum, path) {
+            if (sum === target) {
+                res.push([...path]);
+                return;
+            }
+
+            if (sum > target) {
+                return;
+            }
+
+            for (let i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] === candidates[i - 1]) {
+                continue;
+                }
+
+                path.push(candidates[i]);
+
+                dfs(i + 1, sum + candidates[i], path);
+
+                path.pop();
+            }
+        }
+
+        dfs(0, 0, []);
+
+        return res;
+    }
+}
