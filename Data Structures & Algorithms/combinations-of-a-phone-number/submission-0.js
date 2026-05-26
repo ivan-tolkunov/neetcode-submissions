865f2d1d634b96899e04a655f72a8ae34de35933
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-05-26 12:17:26 -0400

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {string} digits
+     * @return {string[]}
+     */
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    res = [];
+
+    letterCombinations(digits) {
+        if (digits.length === 0) {
+            return this.res;
+        }
+        
+        const map = new Map();
+        let index = 0;
+
+        for (let i = 2; i <= 9; i++) {
+            let val = '';
+
+            if (i === 7 || i === 9) {
+                val = this.alphabet.slice(index, index + 4);
+                index += 4;
+            } else {
+                val = this.alphabet.slice(index, index + 3);
+                index += 3;
+            }
+
+            map.set(`${i}`, val);
+        }
+
+        this.backtrack(digits, 0, '', map);
+
+        return this.res;
+    }
+
+    backtrack(digits, i, str, map) {
+        if (str.length === digits.length) {
+            this.res.push(str);
+            return;
+        }
+
+        const variations = map.get(digits[i]);
+
+        for (let ch of variations) {
+            str += ch;
+            this.backtrack(digits, i + 1, str, map);
+            str = str.slice(0, -1);
+        }
+    }
+}
