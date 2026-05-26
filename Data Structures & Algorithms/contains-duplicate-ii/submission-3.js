c260f8d837b58d2fabb78ef30302385f829b3406
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-05-26 15:09:16 -0400

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {boolean}
+     */
+    containsNearbyDuplicate(nums, k) {
+        const set = new Set();
+        let left = 0;
+
+        for (let right = 0; right < nums.length; right++) {
+            if (set.size > k) {
+                set.delete(nums[left]);
+                left++;
+            }
+
+            if (set.has(nums[right])) {
+                return true;
+            }
+
+            set.add(nums[right]);
+        }
+
+        return false;
+    }
+}
