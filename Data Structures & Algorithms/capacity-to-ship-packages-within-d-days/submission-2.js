c28ff9e28b9a5485bbf3ceba84525782df4b4358
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-06-30 13:10:26 -0400

    Add: capacity-to-ship-packages-within-d-days - submission-2

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-2.js
@@ -0,0 +1,51 @@
+class Solution {
+    /**
+     * @param {number[]} weights
+     * @param {number} days
+     * @return {number}
+     */
+    shipWithinDays(weights, days) {
+        let left = 0;
+        let right = 0;
+
+        for (let weight of weights) {
+            right += weight;
+            left = Math.max(left, weight);
+        }
+
+        let res = right;
+
+        while (left < right) {
+            const mid = left + Math.floor((right - left) / 2);
+
+            if (validate(mid)) {
+                res = mid;
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        function validate(capacity) {
+            let time = 1;
+            let sum = 0;
+
+            for (let weight of weights) {
+                if (sum + weight > capacity) {
+                    time++;
+                    sum = 0;
+                }
+
+                sum += weight;
+
+                if (time > days) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return left;
+    }
+}
