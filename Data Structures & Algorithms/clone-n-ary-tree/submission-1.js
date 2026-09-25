d5114a767d1b73581791d6f34e792e6b8aa374cd
Ivan Tolkunov <57285928+ivan-tolkunov@users.noreply.github.com>
2026-09-25 13:26:43 +0000

    Add: clone-n-ary-tree - submission-1

---
+++ Data Structures & Algorithms/clone-n-ary-tree/submission-1.js
@@ -0,0 +1,42 @@
+/**
+ * // Definition for a Node.
+ * class Node {
+ *     constructor(val, children) {
+ *         this.val = val === undefined ? 0 : val;
+ *         this.children = children === undefined ? [] : children;
+ *     }
+ * }
+ */
+
+class Solution {
+    constructor() {
+        this.map = new Map();
+    }
+    /**
+     * @param {_Node|null} node
+     * @return {_Node|null}
+     */
+    cloneTree(root) {
+        if (!root) {
+            return null;
+        }
+        
+        let clone = this.map.get(root);
+
+        if (!clone) {
+            clone = new Node(root.val, []);
+            this.map.set(root, clone);
+        }
+
+        for (let nei of root.children) {
+            if (this.map.has(nei)) {
+                clone.childre.push(this.map.get(nei));
+                continue;
+            }
+
+            clone.children.push(this.cloneTree(nei));
+        }
+
+        return clone;
+    }
+}
